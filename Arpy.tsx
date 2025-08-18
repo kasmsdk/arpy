e3a36d47c63b6ceafd01ca6acd89adab4f52fb14
Kevin Staunton-Lambert <music@pyrmontbrewery.com.au>
2025-08-18 23:58:58 +1000

    (chore) Basic doco structure

--- Arpy.tsx
+++ Arpy.tsx
@@ -4,9 +4,11 @@ import MidiSelector from '../components/MidiSelector';
 const Arpy: React.FC = () => {
     return (
         <div style={{ padding: '2em' }}>
-            WebMIDI Arpy arpeggios pattern Browser and Editor Tool.<br />
+            <h1>Arpy arpeggio pattern Browser and Editor Tool</h1>
             <p className="sunken-paragraph">
                 Arpy is a collection of arpeggios that are play whilst keys are held or are latched to keep playing out</p>
+            <p className="sunken-paragraph">
+                Arpy sequence browser/gallery and web based editor tools are coming here soon...</p>
             <div style={{ margin: '20px 0' }}>
                 <div>
                     Connect to your MIDI device... (you might need a <a href="https://help.ableton.com/hc/en-us/articles/209774225-Setting-up-a-virtual-MIDI-bus" target="_blank">virtual MIDI bus</a>)
