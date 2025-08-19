48af9a0251b8bc6b79e94f2cb65e6dbc8edd49a7
Kevin Staunton-Lambert <music@pyrmontbrewery.com.au>
2025-08-20 08:44:26 +1000

    (chore) MidiKeys React component

--- Arpy.tsx
+++ Arpy.tsx
@@ -1,23 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import MidiSelector from '../latest/MidiSelector';
 import ArpyCanvas from '../latest/ArpyCanvas';
-import NewsNewFeatures from '../src/components/NewsNewFeatures';
-import LatestDemo from '../src/components/LatestDemo';
+import LatestDemoArpy from '../src/components/LatestDemoArpy';
 import MidiKeyboard from '../src/components/MidiKeyboard';
 
 const Arpy: React.FC = () => {
+    // Store MIDI data in state
+    const [midiData, setMidiData] = useState<{ note: number; velocity: number; isCC: boolean } | null>(null);
     // Handler for note on
     const handleNoteOn = (note: number, velocity: number) => {
         window.inlet_5_emanator = 'Arpy';
-        if (typeof window.update_canvas_data === 'function') {
-            window.update_canvas_data({ note, velocity });
-        }
+        setMidiData({ note, velocity, isCC: false });
+        // if (typeof kasm_rust !== 'undefined' && typeof kasm_rust.update_canvas_data === 'function') {
+        //     kasm_rust.update_canvas_data(note, velocity, false);
+        // }
     };
     // Handler for note off
     const handleNoteOff = (note: number) => {
-        if (typeof window.update_canvas_data === 'function') {
-            window.update_canvas_data({ note, velocity: 0 });
-        }
+        setMidiData({ note, velocity: 0, isCC: false });
+        // if (typeof kasm_rust !== 'undefined' && typeof kasm_rust.update_canvas_data === 'function') {
+        //     kasm_rust.update_canvas_data(note, 0, false);
+        // }
     };
     return (
         <div className="kasm-landing-container">
@@ -25,23 +28,17 @@ const Arpy: React.FC = () => {
             <p>
                 Arpy is a collection of arpeggios that are play whilst keys are held or are latched to keep playing out</p>
             <p>
-                Arpy sequence browser/gallery and web based editor tools are coming here soon...</p>
-            <div style={{ margin: '20px 0' }}>
-                <p>
-                    Connect to your MIDI device... (you might need a <a href="https://help.ableton.com/hc/en-us/articles/209774225-Setting-up-a-virtual-MIDI-bus" target="_blank">virtual MIDI bus</a>)
-                    <MidiSelector />
-                </p>
-            </div>
-            <div style={{ display: 'flex', gap: '20px', justifyContent: 'center', margin: '40px 0' }}>
-                <ArpyCanvas title="Arpy Canvas 1" />
-            </div>
+                Pattern gallery/browser<br/>
+                <ArpyCanvas title="Arpy Canvas 1" midiData={midiData} />
+                <ArpyCanvas title="Arpy Canvas 2" midiData={midiData} />
+                <ArpyCanvas title="Arpy Canvas 3" midiData={midiData} />
+            </p>
+            <MidiSelector/>
             <div style={{ display: 'flex', justifyContent: 'center', margin: '20px 0' }}>
                 <MidiKeyboard onNoteOn={handleNoteOn} onNoteOff={handleNoteOff} />
             </div>
 
-            <NewsNewFeatures />
-            <LatestDemo />
-
+            <LatestDemoArpy />
 
         </div>
     );
