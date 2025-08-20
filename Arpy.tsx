92f888b16b350c3e24a30400f1dba44803fb8677
Kevin Staunton-Lambert <music@pyrmontbrewery.com.au>
2025-08-20 11:10:30 +1000

    (chore) MidiKeys React component

--- Arpy.tsx
+++ Arpy.tsx
@@ -1,26 +1,29 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import MidiSelector from '../latest/MidiSelector';
 import ArpyCanvas from '../latest/ArpyCanvas';
+import type { ArpyCanvasHandle } from '../latest/ArpyCanvas';
 import LatestDemoArpy from '../src/components/LatestDemoArpy';
 import MidiKeyboard from '../src/components/MidiKeyboard';
 
 const Arpy: React.FC = () => {
     // Store MIDI data in state
     const [midiData, setMidiData] = useState<{ note: number; velocity: number; isCC: boolean } | null>(null);
+    // Ref for ArpyCanvas
+    const arpyCanvasRef = useRef<ArpyCanvasHandle>(null);
     // Handler for note on
     const handleNoteOn = (note: number, velocity: number) => {
         window.inlet_5_emanator = 'Arpy';
         setMidiData({ note, velocity, isCC: false });
-        // if (typeof kasm_rust !== 'undefined' && typeof kasm_rust.update_canvas_data === 'function') {
-        //     kasm_rust.update_canvas_data(note, velocity, false);
-        // }
+        if (arpyCanvasRef.current) {
+            arpyCanvasRef.current.callKasmFunction('update_canvas_data', { pitch: note, velocity, cc: false });
+        }
     };
     // Handler for note off
     const handleNoteOff = (note: number) => {
         setMidiData({ note, velocity: 0, isCC: false });
-        // if (typeof kasm_rust !== 'undefined' && typeof kasm_rust.update_canvas_data === 'function') {
-        //     kasm_rust.update_canvas_data(note, 0, false);
-        // }
+        if (arpyCanvasRef.current) {
+            arpyCanvasRef.current.callKasmFunction('update_canvas_data', { pitch: note, velocity: 0, cc: false });
+        }
     };
     return (
         <div className="kasm-landing-container">
@@ -29,7 +32,7 @@ const Arpy: React.FC = () => {
                 Arpy is a collection of arpeggios that are play whilst keys are held or are latched to keep playing out</p>
             <p>
                 Pattern gallery/browser<br/>
-                <ArpyCanvas title="Arpy Canvas 1" midiData={midiData} />
+                <ArpyCanvas ref={arpyCanvasRef} title="Arpy Canvas 1" midiData={midiData} />
                 <ArpyCanvas title="Arpy Canvas 2" midiData={midiData} />
                 <ArpyCanvas title="Arpy Canvas 3" midiData={midiData} />
             </p>
